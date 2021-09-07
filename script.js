ef14306e743fd59651e144023b9e6063360846c1
Dolphy Dhingra <60059298+dolphy17@users.noreply.github.com>
2021-09-07 20:13:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Full Stack Developer", "Android Developer"],
+        strings: ["Web Application Developer", "Mobile Application Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Full Stack Developer", "Android Developer"],
+        strings: ["Web Application Developer", "Mobile Application Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
